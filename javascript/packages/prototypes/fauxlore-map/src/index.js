563a3852871db77b714829d5317e9f7b872d21ad
aleh_kaportsau <aleh.kaportsau@softnetix.io>
2025-05-29 00:43:05 +0300

    refactor

--- javascript/packages/prototypes/fauxlore-map/src/index.js
+++ javascript/packages/prototypes/fauxlore-map/src/index.js
@@ -1,6 +1,6 @@
 import { createElement } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./ui/App";
+import { App } from "./ui/app.js";
 import "./client/ws";
 
 const container = document.getElementById("app");
