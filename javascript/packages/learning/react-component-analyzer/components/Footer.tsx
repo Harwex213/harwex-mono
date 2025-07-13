1ae31f7d836d1a126ad06e2b61db172e844fb2a1
aleh_kaportsau <aleh.kaportsau@softnetix.io>
2025-07-13 18:29:52 +0300

    check imports from libs

--- javascript/packages/learning/react-component-analyzer/components/Footer.tsx
+++ javascript/packages/learning/react-component-analyzer/components/Footer.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { SocialLinks } from './SocialLinks';
+import { SomeExternalComponent } from 'not-real-lib';
 
 export function Footer() {
   return (
     <footer className="footer">
       <p>&copy; 2024 Harwex. All rights reserved.</p>
-      <SocialLinks />
+      <SocialLinks/>
+      <SomeExternalComponent/>
     </footer>
   );
 } 
