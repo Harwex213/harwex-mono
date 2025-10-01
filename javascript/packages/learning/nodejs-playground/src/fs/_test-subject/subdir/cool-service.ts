f17324976264a044813b1a690a78c7980a0983cb
aleh_kaportsau <aleh.kaportsau@softnetix.io>
2025-10-01 15:29:24 +0300

    steup nodejs-playground

---
+++ javascript/packages/learning/nodejs-playground/src/fs/_test-subject/subdir/cool-service.ts
@@ -0,0 +1,13 @@
+const sum = (a: number, b: number) => a + b;
+const dis = (a: number, b: number) => a - b;
+const mul = (a: number, b: number) => a * b;
+const div = (a: number, b: number) => a / b;
+
+const SERVICE = {
+  sum,
+  dis,
+  mul,
+  div
+}
+
+export { SERVICE };
