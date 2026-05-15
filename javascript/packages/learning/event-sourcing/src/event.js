b0c27053e2b44ce7d850f0a08bc8cc0caa358dda
aleh_kaportsau <aleh.kaportsau@softnetix.io>
2026-05-15 17:09:19 +0300

    sqlite practice

---
+++ javascript/packages/learning/event-sourcing/src/event.js
@@ -0,0 +1,12 @@
+const EntityMutationEvent = {
+  id: "407be18c-cad0-4305-b6ef-5b139a8d1a64",
+  when: 1462889308,
+  who: "9f58420c-a9f6-4202-adae-a9cf287a3168",
+  what: "UserCreated",
+  diff: [
+    {
+      columnName: "name",
+      newValue: "Jane"
+    }
+  ]
+};
