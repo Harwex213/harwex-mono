dc7796aa69c8ffb4efc4c4c97c0527a5b5fdc01c
aleh_kaportsau <aleh.kaportsau@softnetix.io>
2026-08-18 15:38:52 +0300

    cylindric coordinate system - animated with sine func

--- unity/SelfLearning/Assets/Scenes/CylindricCoordinateSystem.cs
+++ unity/SelfLearning/Assets/Scenes/CylindricCoordinateSystem.cs
@@ -3,13 +3,26 @@ using UnityEngine;
 
 public class CylindricCoordinateSystem : MonoBehaviour
 {
-    private readonly float _moveSpeed = 10f;
-    public float height;
-    public float radius;
-    public float angle;
+    public float animationTimeSec = 3f;
 
-    void Start()
+    public float startHeight = 0f;
+    public float endHeight = 3f;
+
+    public float startRadius = 0f;
+    public float endRadius = 3f;
+
+    public float startAngle = 0f;
+    public float endAngle = 360f;
+
+    private void Update()
     {
+        var speed = animationTimeSec == 0 ? 0 : Mathf.PI / 2f / this.animationTimeSec;
+        var sineValue = Mathf.Abs(Mathf.Sin(Time.time * speed));
+
+        var height = this.startHeight + (this.endHeight - this.startHeight) * sineValue;
+        var radius = this.startRadius + (this.endRadius - this.startRadius) * sineValue;
+        var angle = this.startAngle + (this.endAngle - this.startAngle) * sineValue * Mathf.PI / 180f;
+
         var position = CylindricCoordinateSystemUtils.CylindricToCartesian(radius, angle, height);
         this.transform.localPosition = position;
     }
